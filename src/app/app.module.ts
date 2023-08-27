bed2e115e31cd3670f953c201ac0f375f956d6a9
Pedrocardo005 <pedrocardo007@hotmail.com>
2023-08-26 21:44:38 -0300

    Cria os componentes que serao das paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './core/layout/top-bar/top-bar.component';
+import { TasksComponent } from './features/tasks/tasks.component';
+import { RegisterComponent } from './features/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    TasksComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
