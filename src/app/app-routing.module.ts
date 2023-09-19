aec3b4a1eb211e72d1676ee0c70af4a8815f6d21
Pedrocardo005 <pedro.cardoso@ssig.tech>
2023-09-18 21:19:04 -0300

    Adiciona rota de tarefas e cadastrar
     - Add tabela na pagina de tarefas
     - Add estilo para tabela

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './features/tasks/tasks.component';
+import { RegisterComponent } from './features/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TasksComponent,
+  },
+  {
+    path: 'cadastrar',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
